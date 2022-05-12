5c6633a9a4ddbc58e1a8900468d36dbe218c8bc3
quantic-teq <payal@quanticteq.com>
2022-05-12 12:01:43 +0530

    Dummy Project

---
+++ shopify.config.js
@@ -0,0 +1,5 @@
+export default {
+  storeDomain: 'quanticteq-shop.myshopify.com',
+  storefrontToken: '320dee8e22240ae8d3bde3579367d1b1',
+  storefrontApiVersion: '2022-04',
+};
---
+++ src/routes/index.server.jsx
@@ -0,0 +1,128 @@
+import {
+  // The `useShopQuery` hook makes server-only GraphQL queries to the Storefront API.
+  useShopQuery,
+  // The `flattenConnection` utility takes Shopify storefront relay data
+  // and transforms it into a flat array of objects.
+  flattenConnection,
+} from '@shopify/hydrogen';
+// Import the `Layout` component that defines the structure of the page.
+import Layout from '../components/Layout.server';
+// Import the `ProductList` component that defines the products to display.
+import ProductList from '../components/ProductList';
+// Import `gql` to parse GraphQL queries.
+import gql from 'graphql-tag';
+
+// Fetch product data from your storefront by passing in a GraphQL query to the
+// `useShopQuery` server component.
+export default function Index() {
+  const {data} = useShopQuery({
+    query: QUERY,
+  });
+
+  // Transform Shopify storefront relay data into
+  // a flat array of objects.
+  const products = flattenConnection(data.products);
+  // Return a list of products.
+  return (
+    <Layout>
+      <ProductList products={products} />
+    </Layout>
+  );
+}
+
+// Define the GraphQL query.
+const QUERY = gql`
+  query HomeQuery {
+    products(first: 150) {
+      edges {
+        node {
+          handle
+          id
+          media(first: 150) {
+            edges {
+              node {
+                ... on MediaImage {
+                  mediaContentType
+                  image {
+                    id
+                    url
+                    altText
+                    width
+                    height
+                  }
+                }
+              }
+            }
+          }
+          metafields(first: 150) {
+            edges {
+              node {
+                id
+                type
+                namespace
+                key
+                value
+                createdAt
+                updatedAt
+                description
+                reference {
+                  __typename
+                  ... on MediaImage {
+                    id
+                    mediaContentType
+                    image {
+                      id
+                      url
+                      altText
+                      width
+                      height
+                    }
+                  }
+                }
+              }
+            }
+          }
+          priceRange {
+            maxVariantPrice {
+              currencyCode
+              amount
+            }
+            minVariantPrice {
+              currencyCode
+              amount
+            }
+          }
+          title
+          variants(first: 150) {
+            edges {
+              node {
+                id
+                title
+                availableForSale
+                image {
+                  id
+                  url
+                  altText
+                  width
+                  height
+                }
+                priceV2 {
+                  currencyCode
+                  amount
+                }
+                compareAtPriceV2 {
+                  currencyCode
+                  amount
+                }
+                selectedOptions {
+                  name
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
